05f8b6b1cbc7d55617f0a97011099cbcf0dea854
Aleksey Kuznetsov <zummenix@gmail.com>
2015-12-28 19:13:43 +0500

    Add bytes method for hhea font table.

--- src/tables/hhea.rs
+++ src/tables/hhea.rs
@@ -55,23 +55,50 @@ impl HHEA {
 
         Ok(hhea)
     }
+
+    #[cfg(test)]
+    fn bytes(&self) -> Vec<u8> {
+        use byteorder::WriteBytesExt;
+
+        let mut data = vec![];
+        data.write_i32::<BigEndian>(self.version.0).unwrap();
+        data.write_i16::<BigEndian>(self.ascent).unwrap();
+        data.write_i16::<BigEndian>(self.descent).unwrap();
+        data.write_i16::<BigEndian>(self.line_gap).unwrap();
+        data.write_u16::<BigEndian>(self.advance_width_max).unwrap();
+        data.write_i16::<BigEndian>(self.min_left_side_bearing).unwrap();
+        data.write_i16::<BigEndian>(self.min_right_side_bearing).unwrap();
+        data.write_i16::<BigEndian>(self.x_max_extent).unwrap();
+        data.write_i16::<BigEndian>(self.caret_slope_rise).unwrap();
+        data.write_i16::<BigEndian>(self.caret_slope_run).unwrap();
+        data.write_i16::<BigEndian>(self.caret_offset).unwrap();
+        data.write_i16::<BigEndian>(self.reserved1).unwrap();
+        data.write_i16::<BigEndian>(self.reserved2).unwrap();
+        data.write_i16::<BigEndian>(self.reserved3).unwrap();
+        data.write_i16::<BigEndian>(self.reserved4).unwrap();
+        data.write_i16::<BigEndian>(self.metric_data_format).unwrap();
+        data.write_u16::<BigEndian>(self.num_of_long_hor_metrics).unwrap();
+        data
+    }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    const SIZE: usize = 16 * 2 + 4;
     const OFFSET: usize = 340;
+    const SIZE: usize = 16 * 2 + 4;
 
     #[test]
     fn runner() {
         let data = super::super::read_file("tests/Tuffy_Bold.ttf");
-        success_read(&data);
+        test_read_write(&data);
     }
 
-    fn success_read(data: &[u8]) {
-        let _ = HHEA::from_data(&data[OFFSET..OFFSET + SIZE]).unwrap();
+    fn test_read_write(data: &[u8]) {
+        let data = &data[OFFSET..OFFSET + SIZE];
+        let hhea = HHEA::from_data(data).unwrap();
+        assert_eq!(hhea.bytes(), data);
     }
 }
 
